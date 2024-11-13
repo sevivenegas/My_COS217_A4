f7d5796dac08bb66a53b30e879095417290ada40
Sevastian Venegas <sv1436@princeton.edu>
2024-11-13 18:41:48 -0500

    dasda

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -86,12 +86,13 @@ static boolean CheckerDT_treeCheck(Node_T oNNode)
             fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
             return FALSE;
          }
-         if(ulIndex + 1 < Node_getNumChildren(oNNode)){
-            if(Path_comparePath(oNChild,Node_getChild(oNNode, ulIndex+1, NULL)) == 0){
+
+         /*if(ulIndex + 1 < Node_getNumChildren(oNNode)){
+            if(Path_comparePath(oNChild, Node_getChild(oNNode, ulIndex+1, NULL)) == 0){
                fprintf(stderr, "duplicate");
                return FALSE;
             }
-         }
+         }*/
          
 
          /* if recurring down one subtree results in a failed check
