fcd95e87bd58925b0c0f9bd70c73a0b57daa9168
Sevastian Venegas <sv1436@princeton.edu>
2024-11-13 13:14:39 -0500

    small fix on line 47 not big change

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -44,7 +44,7 @@ boolean CheckerDT_Node_isValid(Node_T oNNode)
       }
    }
    indexOfChild = 0;
-   if(Node_hasChild(oNParent, oPNPath, &indexOfChild)) {
+   if(oNParent != NULL && Node_hasChild(oNParent, oPNPath, &indexOfChild)) {
          fprintf(stderr, "Sample tests if there is a duplicate that is going to be added change later");
          return FALSE;
    }
