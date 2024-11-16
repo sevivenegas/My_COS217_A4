d4e94ba09ab3402c0622787ce857511e5b76822f
neha <neha.ayyalapu@gmail.com>
2024-11-16 15:55:54 -0500

    some not great attempts at pt 2

--- 0shared/path.c
+++ 0shared/path.c
@@ -340,7 +340,7 @@ size_t Path_getSharedPrefixDepth(Path_T oPPath1, Path_T oPPath2)
    return ulMin;
 }
 
-const char *Path_getComponent(Path_T oPPath, size_t ulLevel)
+static const char *Path_getComponent(Path_T oPPath, size_t ulLevel)
 {
    assert(oPPath != NULL);
 
